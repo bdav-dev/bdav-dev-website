31f62cd691724009b9b5473bed6c53fa265d0c6a
David Berezowski <david.berezowski1@gmail.com>
2024-03-28 13:04:32 +0100

    Removed 'de.' from  LinkedIn link

--- src/components/links/social/LinkedInLink.tsx
+++ src/components/links/social/LinkedInLink.tsx
@@ -4,7 +4,7 @@ import SocialLink from "./SocialLink";
 export default function LinkedInLink() {
 
     return (
-        <SocialLink href={"https://de.linkedin.com/in/david-berezowski-ab5639273"}>
+        <SocialLink href={"https://linkedin.com/in/david-berezowski-ab5639273"}>
             <LinkedInIcon className="fill-white h-7 w-24" />
         </SocialLink>
     );
