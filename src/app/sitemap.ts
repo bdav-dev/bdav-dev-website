4fe2e142f340f25d2e252acbb4d1f08fba2d6b7a
David Berezowski <david.berezowski1@gmail.com>
2026-01-06 17:08:17 +0100

    Removed sitemap options that caused crash at build time

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,73 +1,9 @@
-import { Abstract3DSeriesValues } from "@/content/3d/a3ds/abstract3dSeries";
-import { CodeProjects } from "@/content/code/codeProjects";
-import { recipes } from "@/content/recipes/recipes";
 import { MetadataRoute } from "next";
 import { Route } from "@/utils/RouteUtils";
-import { Abstract3DStaticCollections } from "@/content/3d/a3ds/abstract3dCollections";
-
-type SitemapEntry = {
-    url: string;
-    lastModified?: string | Date;
-    changeFrequency?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
-    priority?: number;
-};
 
 const bdavDev = 'https://www.bdav.dev'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
-    const codeProjectEntries: SitemapEntry[] = Object.values(CodeProjects).map(
-        codeProject => (
-            {
-                url: bdavDev + Route.codeProject(codeProject),
-                lastModified: new Date(),
-                changeFrequency: "yearly",
-                priority: 0.8
-            }
-        )
-    );
-
-    const abstract3DEntries: SitemapEntry[] = Abstract3DSeriesValues.map(
-        abstract3D => (
-            {
-                url: bdavDev + Route.abstract3d(abstract3D),
-                lastModified: new Date(),
-                changeFrequency: "yearly",
-                priority: 0.6
-            }
-        )
-    );
-    const abstract3DCollections: SitemapEntry[] = Object.values(Abstract3DStaticCollections).map(
-        collection => (
-            {
-                url: bdavDev + Route.abstract3dCollection(collection),
-                lastModified: new Date(),
-                changeFrequency: "monthly",
-                priority: 0.7
-            }
-        )
-    );
-    abstract3DCollections.push(
-        {
-            url: bdavDev + Route.abstract3dFavoritesCollection,
-            lastModified: new Date(),
-            changeFrequency: "monthly",
-            priority: 0.7
-        }
-    );
-
-    const recipesEntries: SitemapEntry[] = Object.values(recipes).map(
-        recipe => (
-            {
-                url: bdavDev + Route.recipe(recipe),
-                lastModified: new Date(),
-                changeFrequency: "yearly",
-                priority: 0.6
-            }
-        )
-    );
-
-
     return [
         {
             url: bdavDev,
@@ -112,12 +48,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.75
-        },
-
-
-        ...codeProjectEntries,
-        ...abstract3DEntries,
-        ...abstract3DCollections,
-        ...recipesEntries
+        }
     ];
 }
