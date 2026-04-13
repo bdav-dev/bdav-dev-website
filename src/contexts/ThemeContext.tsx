d58403c56c141e3c414f7b698dbf50722eca7c0c
David Berezowski <david.berezowski1@gmail.com>
2026-04-13 22:26:11 +0200

    ThemeContext: Added resolveThemeSwitch function

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import React, { Dispatch, SetStateAction, useEffect } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import useLocalStorage from "../hooks/UseLocalStorage";
 
 
 type ThemeContextType = {
     darkTheme: boolean,
-    setDarkTheme: Dispatch<SetStateAction<boolean>>
+    setDarkTheme: Dispatch<SetStateAction<boolean>>,
+    resolveThemeSwitch: <T>(themeSwitch: T | { light: T, dark: T }) => T
 }
 
 export const ThemeContext = React.createContext<ThemeContextType>({
     darkTheme: true,
-    setDarkTheme: () => { }
+    setDarkTheme: () => { },
+    resolveThemeSwitch: themeSwitch => resolveThemeSwitch(true, themeSwitch)
 });
 
 type ThemeProviderProps = {
@@ -22,8 +24,15 @@ export default function ThemeProvider(props: ThemeProviderProps) {
     const [darkTheme, setDarkTheme] = useLocalStorage("useDarkTheme", true);
 
     return (
-        <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+        <ThemeContext.Provider value={{ darkTheme, setDarkTheme, resolveThemeSwitch: themeSwitch => resolveThemeSwitch(darkTheme, themeSwitch) }}>
             {props.children}
         </ThemeContext.Provider>
     );
+}
+
+export function resolveThemeSwitch<T>(darkTheme: boolean, themeSwitch: T | { light: T, dark: T }): T {
+    if (typeof themeSwitch === 'object' && themeSwitch != null && 'light' in themeSwitch && 'dark' in themeSwitch) {
+        return darkTheme ? themeSwitch.dark : themeSwitch.light;
+    }
+    return themeSwitch;
 }
