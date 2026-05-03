6e61308d050e2c693d9c29409ba39132b4fde057
David Berezowski <david.berezowski1@gmail.com>
2026-05-03 17:24:11 +0200

    Written Timecoder page

--- src/app/(bdav-dev)/code/timecoder/page.tsx
+++ src/app/(bdav-dev)/code/timecoder/page.tsx
@@ -1,5 +1,13 @@
 import { CodeProjects } from "@/categories/code/content/codeProjects";
 import CodeProjectLayout from "@/categories/code/components/layout/CodeProjectLayout";
+import ReadingLayout from "@/layout/ReadingLayout";
+import CodeProjectTitle from "@/categories/code/components/CodeProjectTitle";
+import Tile from "@/components/library/Tile";
+import { ThemeSwitch } from "@/contexts/ThemeContext";
+import { CloudinaryImage } from "@/cloudinary";
+import SkeletonLoadingCloudinaryImage from "@/components/SkeletonLoadingCloudinaryImage";
+import CodeProjectAction from "@/categories/code/components/CodeProjectAction";
+
 
 export default function TimecoderPage() {
     const project = CodeProjects.Timecoder;
@@ -9,14 +17,33 @@ export default function TimecoderPage() {
             project={project}
             banner={
                 {
-                    image: {
-                        src: '/code/create-blender-project/banner.png',
-                        height: 1080,
-                        width: 2200
-                    },
-                    className: 'invert dark:invert-0 hue-rotate-180 dark:hue-rotate-0 dark:brightness-75'
+                    image: { src: '/code/timecoder/banner.png', height: 3500, width: 1440 },
+                    className: 'brightness-[90%] dark:brightness-[40%]'
                 }
             }
-        />
+        >
+            <ReadingLayout>
+                <div className={'flex flex-col gap-5'}>
+                    <Tile className={'text-center'}>
+                        <CodeProjectTitle project={project}/> is a tool that lets video producers and clients document and comment in/out timecodes.
+                    </Tile>
+
+                    <SkeletonLoadingCloudinaryImage
+                        alt={'Timecoder'}
+                        image={images.timecoder}
+                        sharedClassName={"rounded-2xl"}
+                        draggable={false}
+                    />
+
+                    <Tile className={'flex justify-center'}>
+                        <CodeProjectAction project={project}/>
+                    </Tile>
+                </div>
+            </ReadingLayout>
+        </CodeProjectLayout>
     );
 }
+
+const images = {
+    timecoder: { src: '/code/timecoder/timecoder.png', width: 3763, height: 1733 }
+} as const satisfies Record<string, ThemeSwitch<CloudinaryImage>>;
