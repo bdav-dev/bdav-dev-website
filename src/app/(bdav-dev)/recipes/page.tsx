37d87aec29b0c88e04d6f2201b8b47c2baaa76b4
David Berezowski <david.berezowski1@gmail.com>
2026-05-09 22:16:48 +0200

    recipe page: Recipes are now organized in categories

--- src/app/(bdav-dev)/recipes/page.tsx
+++ src/app/(bdav-dev)/recipes/page.tsx
@@ -1,24 +1,39 @@
 import RecipeLink from "@/components/link/content/RecipeLink";
-import Section from "@/components/layout/document/Section";
 import { Recipes } from "@/categories/recipes/content/recipes";
 import { Metadata } from "next";
+import Article from "@/components/layout/document/Article";
+import Section from "@/components/layout/document/Section";
+import { RecipeCategories, RecipeCategory } from "@/categories/recipes/content/recipeCategory";
+
 
 export const metadata: Metadata = {
-    title: `bdav.dev – Recipes`,
+    title: `Recipes – bdav.dev`,
     description: "Recipes from David Berezowski."
 }
 
 export default function RecipesPage() {
     return (
-        <Section headline="Recipes">
-            <div className="mt-2.5 flex flex-row flex-wrap gap-2.5 justify-center sm:justify-normal">
+        <Article headline={'Recipes'}>
+            <div className={'flex flex-row flex-wrap justify-center sm:justify-normal gap-2.5 mt-2.5'}>
                 {
-                    Object.values(Recipes).map(
-                        (recipe, index) => <RecipeLink recipe={recipe} key={index}/>
-                    )
+                    Object.values(RecipeCategories)
+                        .map((category, index) => <RecipeCategorySection category={category} key={index}/>)
+                }
+            </div>
+        </Article>
+    );
+}
+
+function RecipeCategorySection(props: { category: RecipeCategory }) {
+    return (
+        <Section headline={props.category.pluralLabel} className={'mb-2'}>
+            <div className={"mt-0.5 flex flex-row flex-wrap gap-2.5"}>
+                {
+                    Object.values(Recipes)
+                        .filter(recipe => recipe.category.id === props.category.id)
+                        .map((recipe, index) => <RecipeLink recipe={recipe} key={index}/>)
                 }
             </div>
         </Section>
     );
 }
-
