e38338cd1b31e2b7330553b424033598f2734c26
David Berezowski <david.berezowski1@gmail.com>
2026-05-28 22:14:13 +0200

    Refactored LinkedIn icon

--- src/components/link/social/LinkedInLink.tsx
+++ src/components/link/social/LinkedInLink.tsx
@@ -1,11 +1,11 @@
-import LinkedInIcon from "@/icons/deprecated/LinkedInIcon";
+import LinkedInGlyph from "@/icons/logo/LinkedInGlyph";
 import SocialLink from "./SocialLink";
 
 export default function LinkedInLink() {
 
     return (
         <SocialLink href={"https://linkedin.com/in/david-berezowski-ab5639273"}>
-            <LinkedInIcon className="fill-white h-7 w-24"/>
+            <LinkedInGlyph/>
         </SocialLink>
     );
 }
