e4de7f7e584c9cb6c534e318a7db16ff263b3213
David Berezowski <david.berezowski1@gmail.com>
2026-06-06 23:10:44 +0200

    FollowMeOnInstagramLink: Improvements

--- src/categories/3d/components/FollowMeOnInstagramLink.tsx
+++ src/categories/3d/components/FollowMeOnInstagramLink.tsx
@@ -1,28 +1,21 @@
-'use client';
-
 import Link from "next/link";
 import Phone from "@/components/library/Phone";
 import SkeletonLoadingCloudinaryImage from "@/components/SkeletonLoadingCloudinaryImage";
 import InstagramGlyph from "@/icons/logo/InstagramGlyph";
-import React, { useState } from "react";
 
 
 export default function FollowMeOnInstagramLink() {
     const instagramHref = 'https://www.instagram.com/davidb.3d/';
-
-    const [hover, setHover] = useState(false);
 
     return (
         <div className={'flex flex-col md:flex-row items-center w-fit max-w-full gap-4 md:gap-6'}>
             <Link
                 href={instagramHref}
                 className={'block w-fit max-w-full'}
-                onPointerEnter={() => setHover(true)}
-                onPointerLeave={() => setHover(false)}
             >
                 <Phone
-                    size={'13rem'}
-                    className={`${hover ? 'rotate-3' : 'rotate-0'} transition-transform max-w-full`}
+                    size={'13.5rem'}
+                    className={'transition-transform max-w-full'}
                     screen={
                         <SkeletonLoadingCloudinaryImage
                             alt={'Instagram page of davidb.3d'}
@@ -46,8 +39,6 @@ export default function FollowMeOnInstagramLink() {
                 <Link
                     href={instagramHref}
                     className={'w-fit text-3xl font-semibold flex flex-row gap-1.5'}
-                    onPointerEnter={() => setHover(true)}
-                    onPointerLeave={() => setHover(false)}
                 >
                     <InstagramGlyph className={'text-[0.85em]'}/>
                     Instagram
