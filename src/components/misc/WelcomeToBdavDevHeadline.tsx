cb7784a2af08fc50fc86d960acc9243e5957336c
David Berezowski <david.berezowski1@gmail.com>
2026-06-07 22:53:44 +0200

    WelcomeToBdavDevHeadline: Minor improvements

--- src/components/misc/WelcomeToBdavDevHeadline.tsx
+++ src/components/misc/WelcomeToBdavDevHeadline.tsx
@@ -1,26 +1,28 @@
-import { Space } from "@/components/library/spacing/Space";
-
 export default function WelcomeToBdavDevHeadline() {
-
     return (
-        <h1 className="text-6xl w-fit mx-auto text-center my-12">
-            Welcome to<Space/>
-            <span
-                className={`
-                    font-semibold
-                    text-8xl
-                    text-transparent
-                    bg-clip-text
-                    bg-gradient-to-r
-                    from-[#00AAFF]
-                    via-[#00ffff]
-                    to-[#01FFA4]
-                    brightness-[0.9]
-                    dark:brightness-100
-                `}
-            >
-                bdav.dev
-            </span>
+        <h1 className="text-6xl w-fit">
+            Welcome to <BdavDev/>
         </h1>
+    );
+}
+
+function BdavDev() {
+    return (
+        <span
+            className={`
+                font-semibold
+                text-8xl
+                text-transparent
+                bg-clip-text
+                bg-gradient-to-r
+                from-[#00AAFF]
+                via-[#00ffff]
+                to-[#01FFA4]
+                brightness-[0.9]
+                dark:brightness-100
+            `}
+        >
+                bdav.dev
+        </span>
     );
 }
