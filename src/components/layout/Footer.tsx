6ebb16cf72b0f26cc1fa7338a34f0e98ecac1ff4
David Berezowski <david.berezowski1@gmail.com>
2026-06-07 22:47:00 +0200

    Footer: Improvements

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,42 @@
 import HyperLink from "@/components/library/link/HyperLink";
 import { BdavDev } from "@/routing";
+import Link from "next/link";
+import GitHubInvertocat from "@/icons/logo/GitHubInvertocat";
+import InstagramGlyph from "@/icons/logo/InstagramGlyph";
+import { ReactNode } from "react";
+import LinkedInGlyph from "@/icons/logo/LinkedInGlyph";
+import bdavDevIcon from '@/app/icon.png';
+import Image from "next/image";
+import VerticalRuler from "@/components/library/VerticalRuler";
 
 
 export default function Footer() {
     return (
-        <footer className="flex flex-row items-end gap-1.5 px-1 pt-2 pb-1 text-zinc-400 dark:text-zinc-700">
-            <div className={'flex flex-row gap-1.5 ml-auto text-xs'}>
+        <footer className="flex flex-row items-center gap-1.5 px-1.5 pt-2 pb-1 text-zinc-500 dark:text-zinc-700 text-sm">
+            <Image
+                alt={'bdav.dev icon'}
+                src={bdavDevIcon}
+                className={'opacity-80 w-5 h-5 select-none'}
+                draggable={false}
+                loading={'lazy'}
+                placeholder={'blur'}
+            />
+
+            <VerticalRuler className={'h-4'}/>
+
+            <SocialLink href={'https://github.com/bdav-dev/'}>
+                <GitHubInvertocat className={'text-[0.95em]'}/>
+            </SocialLink>
+
+            <SocialLink href={'https://www.instagram.com/davidb.3d/'}>
+                <InstagramGlyph className={'text-[0.95em]'}/>
+            </SocialLink>
+
+            <SocialLink href={'https://linkedin.com/in/david-berezowski-ab5639273/'}>
+                <LinkedInGlyph className={'text-[0.95em]'}/>
+            </SocialLink>
+
+            <div className={'flex flex-row gap-1.5 ml-auto'}>
                 <HyperLink href={BdavDev.privacyPolicy.getRoute()} discreet>
                     Datenschutzerklärung
                 </HyperLink>
@@ -15,5 +46,13 @@ export default function Footer() {
                 </HyperLink>
             </div>
         </footer>
+    );
+}
+
+function SocialLink(props: { href: string, children?: ReactNode }) {
+    return (
+        <Link href={props.href} className={'inline-flex hover:text-zinc-800 dark:hover:text-zinc-400 transition-colors duration-100'}>
+            {props.children}
+        </Link>
     );
 }
