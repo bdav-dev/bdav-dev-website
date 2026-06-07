f89963c16effd357c44553bcca38f7cd10580ce3
David Berezowski <david.berezowski1@gmail.com>
2026-06-07 22:51:59 +0200

    NewBadge: Added 'small' option

--- src/categories/3d/a3ds/components/NewBadge.tsx
+++ src/categories/3d/a3ds/components/NewBadge.tsx
@@ -1,8 +1,13 @@
-
-
-export default function NewBadge(props: { shadow?: boolean }) {
+export default function NewBadge(props: { shadow?: boolean, small?: boolean }) {
     return (
-        <div className={`font-semibold bg-rose-600 text-white rounded-full px-2 w-fit ${props.shadow && 'shadow-zinc-950/.5 shadow-lg'}`}>
+        <div
+            className={`
+                bg-rose-600 text-white
+                rounded-full w-fit
+                ${props.small ? 'text-xs px-1' : 'font-semibold px-2'}
+                ${props.shadow && 'shadow-zinc-950/.5 shadow-lg'}
+            `}
+        >
             New
         </div>
     );
