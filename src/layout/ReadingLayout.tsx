53f2ab6583db5ae55129d0364680c7a791134688
David Berezowski <david.berezowski1@gmail.com>
2026-06-12 22:26:52 +0200

    ReadingLayout: Removed horizontal margin

--- src/layout/ReadingLayout.tsx
+++ src/layout/ReadingLayout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function ReadingLayout(props: { children?: ReactNode }) {
     return (
-        <div className={"mx-auto px-5 max-w-[90rem]"}>
+        <div className={"mx-auto max-w-[90rem]"}>
             {props.children}
         </div>
     );
