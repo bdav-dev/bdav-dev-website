d74c612ea4a04766a29365312cd17d73241db655
David Berezowski <david.berezowski1@gmail.com>
2026-06-21 13:15:07 +0200

    Added not-found pages to Abstract3D Series

---
+++ src/app/(main)/3d/abstract-3d-series/[nr]/not-found.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import HyperLink from "@/components/library/link/HyperLink";
+import { BdavDev } from "@/library/routing";
+import { usePathname } from "next/navigation";
+
+export default function Abstract3dImageNotFound() {
+    const pathname = usePathname();
+
+    const unknownImage = pathname.split("/").pop();
+
+    return (
+        <div className={'flex-1 flex flex-col items-center justify-center gap-0.5'}>
+            <span>
+                Abstract3D Series does not contain image "{unknownImage}".
+            </span>
+            <HyperLink href={BdavDev.blender.abstract3dSeries.getRoute()}>
+                View Abstract3D Series
+            </HyperLink>
+        </div>
+    );
+}
---
+++ src/app/(main)/3d/abstract-3d-series/collections/[collection]/not-found.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import HyperLink from "@/components/library/link/HyperLink";
+import { BdavDev } from "@/library/routing";
+import { usePathname } from "next/navigation";
+
+export default function Abstract3dCollectionNotFound() {
+    const pathname = usePathname();
+
+    const unknownCollection = pathname.split("/").pop();
+
+    return (
+        <div className={'flex-1 flex flex-col items-center justify-center gap-0.5'}>
+            <span>
+                Abstract3D Series does not contain collection "{unknownCollection}".
+            </span>
+            <HyperLink href={BdavDev.blender.abstract3dSeries.collections.getRoute()}>
+                View collections
+            </HyperLink>
+        </div>
+    );
+}
